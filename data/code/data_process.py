c4830c47fe0a3cd1052193a11837bd171919d7d9
matejur <matej.urbas00@gmail.com>
2024-05-17 12:13:28 +0200

    Speaker extracting - Prompt engineering

--- data/code/data_process.py
+++ data/code/data_process.py
@@ -4,16 +4,29 @@ import re
 
 import pandas as pd
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, pipeline
 
 logging.getLogger("transformers.generation_utils").setLevel(logging.ERROR)
 # supresses 'Setting `pad_token_id` to `eos_token_id`:50256 for open-end generation.'
 
 print("Imported packages")  # this takes forever on HPC
 
-model_name = "TinyLlama/TinyLlama-1.1B-Chat-v0.6"
+model_name = "microsoft/Phi-3-mini-4k-instruct"
 
-model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16,
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    quantization_config=bnb_config,
+    trust_remote_code=True,
+)
+model.config.use_cache = False
+
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 
 device = "cpu"
@@ -25,12 +38,15 @@ elif torch.backends.mps.is_available():
     device = "mps"
     print("MPS is available, Jan is happy")
 
-model = model.to(device)
+# model = model.to(device) # Ne dela z bits and bytes
 
 
 data_path = "./../books/asoif/"
 files = os.listdir(data_path)
 files = [data_path + file for file in files]
+
+with open(os.path.join(data_path, "characters.txt"), "r", encoding="utf-8") as file:
+    characters = ", ".join((map(str.strip, file)))
 
 
 def extract_dialogues_with_context(filepath, context_length=300):
@@ -67,20 +83,26 @@ text_gen = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
 def classify_dialogues(dialogue_entry):
     # print(dialogue_entry)
-    prompt_text = f"I need you to identify who is speaking in the provided dialoge. I will give you context surrounding the dialogue, to help you figure it out. Context: '{dialogue_entry['pre_context']}' Dialogue: '{dialogue_entry['dialogue']}' Following Context: '{dialogue_entry['post_context']}' Who is speaking?"
+    prompt_text = f"""I will give you text before and after a dialogue. Based on this information, you will have to determine who is speaking the dialogue.
 
-    gen = text_gen(prompt_text, max_new_tokens=30)  # [0]["generated_text"].strip()
+### Text before the dialogue: {dialogue_entry['pre_context']} ### End of text before the dialogue.
 
-    print(gen)
+### Text after the dialogue: {dialogue_entry['post_context']} ### End of text after the dialogue.
+Answer with only the cahracter's name. If you are unsure, answer with UNKNOWN.
+Who is speaking the dialogue?
+### Response: """
 
-    result = gen[0]["generated_text"].strip()
-
-    print(result[-50:])
+    gen = text_gen(prompt_text, max_new_tokens=10)  # [0]["generated_text"].strip()
+    print(prompt_text)
+    result = gen[0]["generated_text"]
+    result = result[len(prompt_text) :].strip()
+    result = result.split("\n")[0].replace("### Response: ", "")
+    print(result)
 
     return result
 
 
-df = df.head(1)
+df = df.head(10)
 
 df["character"] = df.apply(classify_dialogues, axis=1)
 
