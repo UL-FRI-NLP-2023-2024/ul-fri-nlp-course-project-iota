d2afadad35158d82546e746875014332b5233446
Žiga Trček <ziga.trc@gmail.com>
2024-05-23 14:18:17 +0200

    Add manual evaluation questions

---
+++ code/evaluation/manual_evaluation_questions.py
@@ -0,0 +1,88 @@
+questions = {
+    'non-specific': [
+        'If you could do anything right now, what would you do?',
+        'Do you like doing sports?',
+        'Are you happy?',
+        'What do you dislike most?',
+        'What makes you happy?',
+        'Describe your values.',
+        'Describe your life journey.',
+    ],
+    'harry_lore': [
+        'Who is your romantic interest?',
+        'What do you think about Professor Snape?',
+        'Who are your best friends?',
+        'What is your favorite spell?',
+        'What is your favorite subject?',
+        'What are you afraid of?',
+        'Who is your favorite teacher?',
+        'Describe your pet.',
+        'What is your Boggart?'
+    ],
+    'Harry': [
+        'When did you first meet Lord Voldemort?',
+        'What happened when Professor Dumbledore first invited you to his office?',
+        'Who is your godfather?',
+        'What is your Patronus?',
+        'Describe your relationship with the Dursleys.',
+        'How do you feel about being famous?',
+    ],
+    'Hermione': [
+        'Tell me about your parents.',
+        'What is your favorite book?',
+        'Who did you go to the Yule Ball with?',
+        'Did you ever lie to a teacher?',
+    ],
+    'Ron': [
+        'Describe your family.',
+        'What chocolate frog card are you missing?',
+        'What is your favorite Quidditch team?',
+        'What is your favorite food?',
+    ],
+    'Dumbledore': [
+        'What is your favorite memory?',
+        'What is your favorite candy?',
+        'Describe your relationship with Professor Snape.',
+        'Why is your hand black?',
+    ],
+    'Voldemort': [
+        'What is your favorite Horcrux?',
+        'What is your favorite spell?',
+        'How did you learn about the Dark Arts?',
+        'Describe your meeting with the Potter family.',
+    ],
+    'Draco': [
+        'What do you think about Harry Potter?',
+        'Who are your friends?',
+        'What is your father like?',
+    ],
+    'asoiaf_lore': [
+        'What do you know about the Starks?',
+        'What do you know about the Lannisters?',
+        'What do you know about the Targaryens?',
+        'What house do you belong to?',
+        'What do you know about The Wall?',
+        'What do you know about the Night\'s Watch?',
+    ],
+    'Hodor': [
+        'What is your favorite word?',
+        'What is your favorite food?',
+        'What is your favorite color?',
+    ],
+    'Tyrion': [
+        'How tall are you?',
+        'What do you think about King Joffrey?',
+        'What is your favorite drink?',
+    ],
+    'Ned': [
+        'Is the winter coming?',
+        'What is your favorite season?',
+        'What is your favorite sword?',
+    ],
+    'Daenerys': [
+        'List your titles.',
+        'What is your favorite dragon?',
+        'Who is your favorite advisor?',
+        'Who is the bravest person you know?',
+    ]
+}
